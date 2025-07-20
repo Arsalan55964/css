5df63d51c3cca0a4beeec422766045e8f854559b
js-arsalan <oarsalan17@gmail.com>
2025-07-20 16:30:22 +0500

    text

---
+++ text1.js
@@ -0,0 +1,14 @@
+
+console.log("hello world")
+   const text = "hello wolrd"
+   const text2 = "hello world"
+   console.log(text2);
+   
+   const text3 = new String("hy tea")
+   console.log(text3);
+   console.log(text3.length);
+   console.log(text3[0]);
+   console.log(text3.__proto__);
+   console.log(text3.toUpperCase());
+   console.log(text3.charAt(2));
+   
