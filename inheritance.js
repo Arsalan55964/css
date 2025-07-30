52341bf9b575d4a5feb9a483bf67da17daf59a38
js-arsalan <oarsalan17@gmail.com>
2025-07-31 00:04:22 +0500

    inheritance

---
+++ inheritance.js
@@ -0,0 +1,43 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+    logMe(){
+        console.log(`USERNAME IS ${this.username}`);
+        
+    }
+}
+
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "123")
+
+chai.logMe()
+const masalaChai = new User("masalaChai")
+
+masalaChai.logMe()
+ 
+console.log(chai instanceof User);           
+
+const tinderUser = {
+    name: "arsalan",
+    id: 1,
+    isLogged: true,
+    lastActive: "today",
+    email: "dd2Email.com",
+    age: 18
+}   
+console.log(tinderUser.name);
+console.log(tinderUser.id);     
+
