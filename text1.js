6ecb684cfac2029ac8800a4c7aa3664863d50ef0
js-arsalan <oarsalan17@gmail.com>
2025-09-20 21:45:53 +0500

    text1

--- text1.js
+++ text1.js
@@ -1,4 +1,3 @@
-
 console.log("hello world")
    const text = "hello wolrd"
    const text2 = "hello world"
@@ -12,3 +11,19 @@ console.log("hello world")
    console.log(text3.toUpperCase());
    console.log(text3.charAt(2));
    
+
+   console.log(text3.indexOf("t"));
+   console.log(text3.lastIndexOf("t"));
+   const text4 = "hello world"
+   console.log(text4.indexOf("l", 3));
+   console.log(text4.lastIndexOf("l", 3));
+   console.log(text4.indexOf("z")); // -1 if not found
+   console.log(text4.includes("l"));   
+   console.log(text4.includes("z"));   
+   console.log(text4.startsWith("h")); 
+   console.log(text4.endsWith("d"));
+   console.log(text4.startsWith("l", 2));
+   console.log(text4.endsWith("l", 3));
+   console.log(text4.slice(0, 5));
+   
+    
